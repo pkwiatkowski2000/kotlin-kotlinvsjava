e26bcb70789a5f7bd1d11b52fe37b6e3015eed47
pkwiatkowski2000 <pawel.kwiatkowski2000@gmail.com>
2023-05-15 19:15:59 +0200

    15.05.2023

---
+++ src/main/kotlin/PierwszyProgram.kt
@@ -0,0 +1,9 @@
+//public class PierwszyProgram {
+//    public static void main() {
+//        System.out.println("Pierwszy program w kotlinie");
+//    }
+//}
+
+fun main() {
+    println("Pierwszy program w Kotlinie")
+}
