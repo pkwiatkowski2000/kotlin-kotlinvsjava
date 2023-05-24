25228081bb31226060d34599e33810e0f9292ba9
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-05-24 14:43:28 +0200

    24.05.2023

--- src/main/kotlin/_4_Tablice/TabliceDwuwymiarowe.kt
+++ src/main/kotlin/_4_Tablice/TabliceDwuwymiarowe.kt
@@ -1,5 +1,13 @@
 package _4_Tablice
 
 fun main() {
-    var tablicaDwuwymiarowa = arrayOf()
+    var tablicaDwuwymiarowa = arrayOf(
+        intArrayOf(1, 2, 3),
+        intArrayOf(4, 5, 6),
+        intArrayOf(7, 8, 9),
+        intArrayOf(10, 11, 12),
+    )
+
+    println(tablicaDwuwymiarowa[1][1])
+    println(tablicaDwuwymiarowa[3][2])
 }
