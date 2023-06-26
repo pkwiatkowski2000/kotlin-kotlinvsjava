bbeb4d92f4f24aec544e98a4bb26e693498cf84a
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-06-26 09:48:19 +0200

    26.06.2023

--- src/main/kotlin/_9_Klasy/Osoba.kt
+++ src/main/kotlin/_9_Klasy/Osoba.kt
@@ -2,11 +2,16 @@ package _9_Klasy
 
 class Osoba4(var imie: String? = null, var nazwisko: String? = null) {
 
-//    var nazwa: String? = null
-//
-//    init {
-//        nazwa = "$imie $nazwisko"
-//    }
+    var nazwa: String? = null
+        get() {
+            return "$imie $nazwisko = wypisano z gettera"
+        }
+        private set
+
+    var budzet: Double = 0.0
+        set(wartosc) {
+            field += wartosc
+        }
 
     fun przedstawSie() {
         println("Jestem $imie $nazwisko")
@@ -14,5 +19,11 @@ class Osoba4(var imie: String? = null, var nazwisko: String? = null) {
 }
 
 fun main() {
+    var osoba = Osoba4("Jan", "Kowalski")
+    println(osoba.nazwa)
+//    osoba.nazwa = "Nikt"
 
+    osoba.budzet = 1000.0
+    osoba.budzet = 2000.0
+    println(osoba.budzet)
 }
