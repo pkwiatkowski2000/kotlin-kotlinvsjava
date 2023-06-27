3ed9a4b68aeb6695f9d630b01b3543f0a946ea66
pkwiat <pawel.kwiatkowski2000@gmail.com>
2023-06-27 08:41:36 +0200

    27.06.2023

--- src/main/kotlin/_10_Dziedziczenie/Pracownik.kt
+++ src/main/kotlin/_10_Dziedziczenie/Pracownik.kt
@@ -2,8 +2,6 @@ package _10_Dziedziczenie
 
 open class Pracownik(var imie: String, var nazwisko: String, var obowiazki: String, var pensja: Int) {
 
-    }
-
     fun odmeldujSie() {
         println("Jestem $imie $nazwisko. Moje obowiązki to $obowiazki" )
     }
--- src/main/kotlin/_10_Dziedziczenie/Programista.kt
+++ src/main/kotlin/_10_Dziedziczenie/Programista.kt
@@ -1,15 +1,11 @@
 package _10_Dziedziczenie
 
-class Programista : Pracownik {
-    var jezykProgramowania: String? = null
+class Programista(
+    imie: String,
+    nazwisko: String,
+    obowiazki: String,
+    pensja: Int,
+    var jezykProgramowania: String
+    ) : Pracownik(imie, nazwisko, obowiazki, pensja) {
 
-    constructor(
-        imie: String,
-        nazwisko: String,
-        obowiazki: String,
-        pensja: Int,
-        jezykProgramowania: String
-    ) : super(imie, nazwisko, obowiazki, pensja) {
-        this.jezykProgramowania = jezykProgramowania
     }
-}
